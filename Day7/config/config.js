cdd8344f8442ee6b024aaa3cec804c8ab9581835
Ryeol93 <cksduf_93@naver.com>
2022-11-25 12:26:29 +0900

    Update config.js

--- Day7/config/config.js
+++ Day7/config/config.js
@@ -3,8 +3,8 @@ module.exports = {
     db_url: 'mongodb://127.0.0.1:27017/frontend',
     db_schemas: [{file:'./member_schema', collection:'member2', schemaName: 'MemberScheam', modelName: 'MemberModel'}],
     facebook: {
-        clientID: '1195875968023442',
-        clientSecret: '21b0e2fd31753ac87ef618c017b5d712',
+        clientID: '',
+        clientSecret: '',
         callbackURL: 'http://127.0.0.1:3000/auth/facebook/callback'
     }
 }
